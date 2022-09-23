d66778f1f8c114e8ccdf3e93be8417e3f47fc7ab
hehepeng <18846042826@163.com>
2022-09-23 18:04:24 +0800

    去除无用代码

--- src/main/java/com/security/controller/UserController.java
+++ src/main/java/com/security/controller/UserController.java
@@ -22,25 +22,7 @@ public class UserController {
 
     @Autowired
     private JwtUtils jwtUtils;
-
-    @PostMapping("/register")
-    public ResponseResult register(@RequestBody LoginRegisterForm form) {
-        jwtTokenUserDetailsService.registerUser(form);
-
-        Map<String, Object> map = new HashMap<>();
-        return ResponseResult.builder().code(CodeEnum.SUCCESS.getCode()).data(map);
-    }
-
-    @PostMapping("/login")
-    public ResponseResult login(@RequestBody LoginRegisterForm form) {
-        jwtTokenUserDetailsService.loadUserByUsername(form.getUsername());
-        Map<String, Object> map = new HashMap<>();
-        //生成令牌
-        String accessToken = jwtUtils.createToken(form.getUsername());
-        map.put("access-token", accessToken);
-        return ResponseResult.builder().code(CodeEnum.SUCCESS.getCode()).data(map);
-    }
-
+    
     @GetMapping("/getUserInfo")
     public ResponseResult getUserInfo(@RequestParam String username) {
         return ResponseResult.builder().code(CodeEnum.SUCCESS.getCode());
