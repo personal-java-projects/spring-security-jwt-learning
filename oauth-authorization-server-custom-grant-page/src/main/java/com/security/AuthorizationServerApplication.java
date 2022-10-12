9cad68f0debd8d7a91b4bab5905ccf231328a34f
hehepeng <18846042826@163.com>
2022-10-12 15:00:54 +0800

    springboot新增支持前端请求地址不存在时的错误页面，统一返回404页面

--- oauth-authorization-server-custom-grant-page/src/main/java/com/security/AuthorizationServerApplication.java
+++ oauth-authorization-server-custom-grant-page/src/main/java/com/security/AuthorizationServerApplication.java
@@ -2,6 +2,11 @@ package com.security;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.server.ConfigurableWebServerFactory;
+import org.springframework.boot.web.server.ErrorPage;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
 
 @SpringBootApplication
 public class AuthorizationServerApplication {
@@ -10,4 +15,12 @@ public class AuthorizationServerApplication {
 		SpringApplication.run(AuthorizationServerApplication.class, args);
 	}
 
+	@Bean
+	public WebServerFactoryCustomizer<ConfigurableWebServerFactory> webServerFactoryCustomizer() {
+		//第二种写法：java8 lambda写法
+		return (factory -> {
+			ErrorPage errorPage404 = new ErrorPage(HttpStatus.NOT_FOUND, "/index.html");
+			factory.addErrorPages(errorPage404);
+		});
+	}
 }
