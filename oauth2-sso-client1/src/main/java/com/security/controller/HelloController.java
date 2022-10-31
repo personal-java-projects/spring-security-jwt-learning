fee5c5e47e26d0c7b52980b8a5d50b32f05375a0
hehepeng <18846042826@163.com>
2022-10-31 15:20:59 +0800

    client1新增getCurrentUser接口

--- oauth2-sso-client1/src/main/java/com/security/controller/HelloController.java
+++ oauth2-sso-client1/src/main/java/com/security/controller/HelloController.java
@@ -3,6 +3,7 @@ package com.security.controller;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
@@ -13,5 +14,10 @@ public class HelloController {
     public String hello() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         return authentication.getName() + Arrays.toString(authentication.getAuthorities().toArray());
+    }
+
+    @RequestMapping("/getCurrentUser")
+    public Object getCurrentUser(Authentication authentication) {
+        return authentication;
     }
 }
