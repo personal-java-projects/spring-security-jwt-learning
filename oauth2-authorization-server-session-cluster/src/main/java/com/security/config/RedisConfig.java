e67d3935d3800930194f6cc76350680645262665
hehepeng <18846042826@163.com>
2022-11-28 13:21:45 +0800

    不再使用fastjson对spring-session进行序列化。解决了Jackson2JsonRedisSerializer对spring-session的反序列化失败的问题

--- oauth2-authorization-server-session-cluster/src/main/java/com/security/config/RedisConfig.java
+++ oauth2-authorization-server-session-cluster/src/main/java/com/security/config/RedisConfig.java
@@ -1,10 +1,9 @@
 package com.security.config;
 
-import com.fasterxml.jackson.annotation.JsonAutoDetect;
-import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.jsontype.impl.LaissezFaireSubTypeValidator;
-import com.security.serializer.FastJsonRedisSerializer;
+import com.fasterxml.jackson.databind.jsontype.BasicPolymorphicTypeValidator;
 import org.springframework.cache.annotation.CachingConfigurerSupport;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.interceptor.KeyGenerator;
@@ -15,18 +14,18 @@ import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
-import org.springframework.session.SessionRepository;
-import org.springframework.session.data.redis.RedisOperationsSessionRepository;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.jackson2.CoreJackson2Module;
+import org.springframework.security.jackson2.SecurityJackson2Modules;
+import org.springframework.security.jackson2.SimpleGrantedAuthorityMixin;
+import org.springframework.security.web.jackson2.WebJackson2Module;
+import org.springframework.security.web.jackson2.WebServletJackson2Module;
+import org.springframework.security.web.server.jackson2.WebServerJackson2Module;
 
-import java.time.Duration;
+import java.time.temporal.Temporal;
+import java.util.Collection;
+import java.util.Map;
 
-/**
- * redis 配置
- *
- * @author WangBin
- * @version V1.0
- * @date 2017.12.05
- */
 @Configuration
 @EnableCaching
 public class RedisConfig extends CachingConfigurerSupport {
@@ -45,30 +44,97 @@ public class RedisConfig extends CachingConfigurerSupport {
         };
     }
 
-    /**
-     * 设置 redisTemplate 序列化方式
-     *
-     * @param factory
-     * @return
-     */
+
     @Bean
-    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
-        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+    public RedisTemplate<?, ?> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        RedisTemplate<?, ?> redisTemplate = new RedisTemplate<>();
         redisTemplate.setConnectionFactory(redisConnectionFactory);
-        // 设置值（value）的序列化采用FastJsonRedisSerializer。
-        FastJsonRedisSerializer<Object> fastJsonRedisSerializer = new FastJsonRedisSerializer<>(Object.class);
-        redisTemplate.setValueSerializer(fastJsonRedisSerializer);
-        redisTemplate.setHashValueSerializer(fastJsonRedisSerializer);
+
+        Jackson2JsonRedisSerializer<Object> jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer<>(Object.class);
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        // 构建白名单，解决SecurityUser中的Collection类型无法序列化和反序列化的问题
+        BasicPolymorphicTypeValidator basicPolymorphicTypeValidator = BasicPolymorphicTypeValidator.builder()
+                .allowIfSubType(Collection.class)
+                .allowIfSubType(Number.class)
+                .allowIfSubType(Map.class)
+                .allowIfSubType(Temporal.class)
+                .allowIfSubType(Object.class)
+                .allowIfSubTypeIsArray()
+                .build();
+        objectMapper.activateDefaultTyping(basicPolymorphicTypeValidator, ObjectMapper.DefaultTyping.NON_FINAL, JsonTypeInfo.As.PROPERTY);
+
+        // 解决SecurityUser中没有Set方法导致的序列化失败的错误
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+
+        /**
+         * 解决如下问题：
+         * org.springframework.data.redis.serializer.SerializationException:
+         * Could not read JSON: Cannot construct instance of `org.springframework.security.web.savedrequest.DefaultSavedRequest`
+         * (no Creators, like default constructor, exist): cannot deserialize from Object value (no delegate- or property-based Creator)
+         */
+        objectMapper.registerModules(SecurityJackson2Modules.getModules(getClass().getClassLoader()));
+
+        jackson2JsonRedisSerializer.setObjectMapper(objectMapper);
+
+        redisTemplate.setValueSerializer(jackson2JsonRedisSerializer);
+        redisTemplate.setHashValueSerializer(jackson2JsonRedisSerializer);
+
+        StringRedisSerializer stringRedisSerializer = new StringRedisSerializer();
         // 设置键（key）的序列化采用StringRedisSerializer。
-        redisTemplate.setKeySerializer(new StringRedisSerializer());
-        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
-        redisTemplate.setDefaultSerializer(fastJsonRedisSerializer);
+        redisTemplate.setKeySerializer(stringRedisSerializer);
+        redisTemplate.setHashKeySerializer(stringRedisSerializer);
+
         redisTemplate.afterPropertiesSet();
+
         return redisTemplate;
     }
 
+    /**
+     * 必须在这里进行session进行redis的序列化配置
+     *
+     * @return
+     */
+    @Bean
+    public RedisSerializer<?> springSessionDefaultRedisSerializer(RedisTemplate<?, ?> redisTemplate) {
+//        Jackson2JsonRedisSerializer<Object> jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer<>(Object.class);
+//
+//        ObjectMapper objectMapper = new ObjectMapper();
+////        objectMapper.registerModule(new CoreJackson2Module());
+////        objectMapper.registerModule(new WebJackson2Module());
+////        objectMapper.registerModule(new WebServerJackson2Module());
+////        objectMapper.registerModule(new WebServletJackson2Module());
+////        objectMapper.registerModule(new OAuth2ClientJackson2Module());
+//
+//        // 构建一个基本的白名单
+//        BasicPolymorphicTypeValidator basicPolymorphicTypeValidator = BasicPolymorphicTypeValidator.builder()
+//                .allowIfSubType(Collection.class)
+//                .allowIfSubType(Number.class)
+//                .allowIfSubType(Map.class)
+//                .allowIfSubType(Temporal.class)
+//                .allowIfSubType(Object.class)
+//                .allowIfSubTypeIsArray()
+//                .build();
+//        objectMapper.activateDefaultTyping(basicPolymorphicTypeValidator, ObjectMapper.DefaultTyping.NON_FINAL, JsonTypeInfo.As.PROPERTY);
+//
+//        // 解决SecurityUser中没有Set方法导致的序列化失败的错误
+//        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+//
+//        /**
+//         * 解决如下问题：
+//         * org.springframework.data.redis.serializer.SerializationException:
+//         * Could not read JSON: Cannot construct instance of `org.springframework.security.web.savedrequest.DefaultSavedRequest`
+//         * (no Creators, like default constructor, exist): cannot deserialize from Object value (no delegate- or property-based Creator)
+//         */
+//        objectMapper.registerModules(SecurityJackson2Modules.getModules(getClass().getClassLoader()));
+//
+//        jackson2JsonRedisSerializer.setObjectMapper(objectMapper);
+//
+//        return jackson2JsonRedisSerializer;
+        return redisTemplate.getValueSerializer();
+    }
 
-//    /**
+    //    /**
 //     * 设置spring session redis 序列化方式
 //     * 这种序列化配置方式，返回值必须是RedisTemplate<Object, Object>
 //     * @param factory
@@ -82,14 +148,4 @@ public class RedisConfig extends CachingConfigurerSupport {
 //        sessionRepository.setDefaultMaxInactiveInterval(36000);
 //        return sessionRepository;
 //    }
-
-    /**
-     * 必须在这里进行session进行redis的序列化配置
-     *
-     * @return
-     */
-    @Bean("springSessionDefaultRedisSerializer")
-    public RedisSerializer<Object> springSessionDefaultRedisSerializer() {
-        return new FastJsonRedisSerializer<>(Object.class);
-    }
 }
