dece2ca68bf2d864f798f26c6f9bc4c445e4de87
hehepeng <18846042826@163.com>
2022-12-08 14:50:49 +0800

    新增无效授权码异常处理，重写oauth2的异常响应

--- oauth2-authorization-server-asymmetrical-keypair/src/main/java/com/security/exception/GlobalExceptionHandler.java
+++ oauth2-authorization-server-asymmetrical-keypair/src/main/java/com/security/exception/GlobalExceptionHandler.java
@@ -6,6 +6,7 @@ import com.security.utils.ResponseResult;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.security.oauth2.common.exceptions.ClientAuthenticationException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -27,14 +28,16 @@ public class GlobalExceptionHandler {
     /**
      * 处理自定义的业务异常
      *
-     * @param req
+     * @param request
      * @param e
      * @return
      */
     @ExceptionHandler(value = BizException.class)
     @ResponseBody
-    public ResponseResult bizExceptionHandler(HttpServletRequest req, BizException e) {
-        logger.error("发生业务异常！原因是：", e.getLocalizedMessage());
+    public ResponseResult bizExceptionHandler(HttpServletRequest request, HttpServletResponse response, BizException e) {
+        logger.error("发生业务异常！原因是：" + e.getLocalizedMessage());
+
+        response.setStatus(e.getErrorCode());
 
         return ResponseResult.builder().code(e.getErrorCode()).message(e.getMessage()).build();
     }
@@ -42,25 +45,28 @@ public class GlobalExceptionHandler {
     /**
      * 处理空指针的异常
      *
-     * @param req
+     * @param request
      * @param e
      * @return
      */
     @ExceptionHandler(value = NullPointerException.class)
     @ResponseBody
-    public ResponseResult exceptionHandler(HttpServletRequest req, NullPointerException e) {
-        logger.error("发生空指针异常！原因是: ", e.getLocalizedMessage());
+    public ResponseResult exceptionHandler(HttpServletRequest request, HttpServletResponse response, NullPointerException e) {
+        logger.error("发生空指针异常！原因是: " + e.getLocalizedMessage());
+
+        response.setStatus(CodeEnum.BODY_NOT_MATCH.getCode());
 
         return ResponseResult.builder().code(CodeEnum.BODY_NOT_MATCH.getCode()).message(Messages.NULL_EXCEPTION).build();
     }
 
     @ExceptionHandler(HttpMessageNotReadableException.class)
     @ResponseBody
-    public ResponseResult requestInvalid() {
-        Map<String, Object> map = new HashMap<>();
-        map.put("code", 404);
-        map.put("msg", "请求输入参数格式不正确");
-        return null;
+    public ResponseResult requestInvalid(HttpServletRequest request, HttpServletResponse response, HttpMessageNotReadableException e) {
+        logger.info("Http 消息不可读异常: " + e.getLocalizedMessage());
+
+        response.setStatus(CodeEnum.BODY_NOT_MATCH.getCode());
+
+        return ResponseResult.builder().code(CodeEnum.BODY_NOT_MATCH.getCode()).message(CodeEnum.BODY_NOT_MATCH.getMessage()).build();
     }
 
     /**
@@ -71,8 +77,8 @@ public class GlobalExceptionHandler {
      * @param e
      * @return
      */
-    @ResponseBody
     @ExceptionHandler(value = MethodArgumentNotValidException.class)
+    @ResponseBody
     public ResponseResult exceptionHandler(HttpServletRequest request, HttpServletResponse response, MethodArgumentNotValidException e) {
         logger.error("参数校验异常！原因是: " + e.getLocalizedMessage());
 
@@ -81,18 +87,19 @@ public class GlobalExceptionHandler {
         return ResponseResult.builder().code(CodeEnum.INTERNAL_SERVER_ERROR.getCode()).message(e.getBindingResult().getFieldError().getDefaultMessage()).build();
     }
 
-
     /**
      * 处理其他异常
      *
-     * @param req
+     * @param request
      * @param e
      * @return
      */
     @ExceptionHandler(value = Exception.class)
     @ResponseBody
-    public ResponseResult exceptionHandler(HttpServletRequest req, Exception e) {
-        logger.error("未知异常！原因是: ", e.getLocalizedMessage());
+    public ResponseResult exceptionHandler(HttpServletRequest request, HttpServletResponse response, Exception e) {
+        logger.error("未知异常！原因是: " + e.getLocalizedMessage());
+
+        response.setStatus(CodeEnum.INTERNAL_SERVER_ERROR.getCode());
 
         return ResponseResult.builder().code(CodeEnum.INTERNAL_SERVER_ERROR.getCode()).message(e.getMessage()).build();
     }
--- oauth2-authorization-server-asymmetrical-keypair/src/main/java/com/security/interceptor/EndpointHandlerInterceptor.java
+++ oauth2-authorization-server-asymmetrical-keypair/src/main/java/com/security/interceptor/EndpointHandlerInterceptor.java
@@ -1,11 +1,10 @@
 package com.security.interceptor;
 
+import com.security.enums.CodeEnum;
+import com.security.utils.ResponseResult;
 import com.security.utils.ResponseUtils;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.security.oauth2.common.exceptions.InvalidClientException;
-import org.springframework.security.oauth2.common.exceptions.InvalidScopeException;
-import org.springframework.security.oauth2.common.exceptions.RedirectMismatchException;
-import org.springframework.security.oauth2.common.exceptions.UnsupportedResponseTypeException;
+import org.springframework.security.oauth2.common.exceptions.*;
 import org.springframework.security.oauth2.provider.NoSuchClientException;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
@@ -35,34 +34,36 @@ public class EndpointHandlerInterceptor implements HandlerInterceptor {
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
         log.info("EndpointHandlerInterceptor afterCompletion");
 
-        Map<String, Object> map = new HashMap<>(4);
+        String message = CodeEnum.INTERNAL_SERVER_ERROR.getMessage();
 
         // 处理client_id不存在的异常
         if (ex instanceof InvalidClientException) {
             String clientId = request.getParameter("client_id");
-            map.put("msg", String.format("client id 无效: %s", clientId));
 
-            ResponseUtils.response(response, map);
+            message = String.format("client id 无效: %s", clientId);
         } else if (ex instanceof NoSuchClientException) {
             String clientId = request.getParameter("client_id");
-            map.put("msg", String.format("client id 不存在: %s", clientId));
 
-            ResponseUtils.response(response, map);
+            message = String.format("client id 不存在: %s", clientId);
         } else if (ex instanceof RedirectMismatchException) {
             String redirectUri = request.getParameter("redirect_uri");
-            map.put("msg", String.format("redirect_uri 不匹配: %s", redirectUri));
 
-            ResponseUtils.response(response, map);
+            message = String.format("redirect_uri 不匹配: %s", redirectUri);
         } else if (ex instanceof UnsupportedResponseTypeException) {
             String responseType = request.getParameter("response_type");
-            map.put("msg", String.format("response_type 不支持: %s", responseType));
 
-            ResponseUtils.response(response, map);
+            message = String.format("response_type 不支持: %s", responseType);
         } else if (ex instanceof InvalidScopeException) {
             String scope = request.getParameter("scope");
-            map.put("msg", String.format("scope 不支持: %s", scope));
 
-            ResponseUtils.response(response, map);
+            message = String.format("scope 不支持: %s", scope);
         }
+        else if (ex instanceof InvalidGrantException) {
+            String code = request.getParameter("code");
+
+            message = String.format("无效 authorization code: %s", code);
+        }
+
+        ResponseUtils.result(response, ResponseResult.builder().fail(message).build());
     }
 }
