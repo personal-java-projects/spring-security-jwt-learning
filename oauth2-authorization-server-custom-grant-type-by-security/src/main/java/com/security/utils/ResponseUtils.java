747cd363c7eb56f7211e63d00d8648af150c059e
hehepeng <18846042826@163.com>
2022-12-09 13:08:21 +0800

    删除使用response.getWriter()的响应工具方法，以解决getWriter() has already been called for this response的问题。

--- oauth2-authorization-server-custom-grant-type-by-security/src/main/java/com/security/utils/ResponseUtils.java
+++ oauth2-authorization-server-custom-grant-type-by-security/src/main/java/com/security/utils/ResponseUtils.java
@@ -19,18 +19,4 @@ public class ResponseUtils {
         out.flush();
         out.close();
     }
-
-    @SuppressWarnings({"deprecation"})
-    public static void response(HttpServletResponse response, Map map) throws IOException {
-        response.setContentType(MediaType.APPLICATION_JSON_UTF8.toString());
-
-        ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.registerModule(new JavaTimeModule());
-
-        String json = objectMapper.writeValueAsString(map);
-
-        response.getWriter().println(json);
-        response.setStatus(HttpServletResponse.SC_OK);
-        response.flushBuffer();
-    }
 }
